0a0c74ad8b2b64fcf970396ecf9f422f71ec73ba
nurbek <torbayevnurbek1992@gmail.com>
2021-04-22 11:49:04 +0600

    validate rules criteria

--- project/src/Controller/ValidateController.php
+++ project/src/Controller/ValidateController.php
@@ -25,6 +25,7 @@ class ValidateController extends LayoutController
         $bin        = (string) $this->f('bin');
         $rules      = $this->f('rule');
         $expiration = (bool) $this->f('expiration', true);
+        $criteria   = (string) $this->f('criteria', 'and');
 
         if (!$rules) {
             $this->forward(
@@ -39,11 +40,11 @@ class ValidateController extends LayoutController
         }
 
         $verify_ocsp = true;
-        $verify_crl = true;
+        $verify_crl  = true;
 
         if (!$expiration) {
             $verify_ocsp = false;
-            $verify_crl = false;
+            $verify_crl  = false;
         }
 
         if (!$cms && !$xml) {
@@ -74,7 +75,7 @@ class ValidateController extends LayoutController
                 $this->forward('error', 'badRequest', [$this->t('error.certificate_is_expired')]);
             }
 
-            $result = false;
+            $statues = [];
 
             foreach ($rules as $rule) {
                 if (!in_array($rule, self::RULES)) {
@@ -94,9 +95,7 @@ class ValidateController extends LayoutController
                         $sign_iin = $ncanode->getIinByXml($xml, $verify_ocsp, $verify_crl);
                     }
 
-                    if ($sign_iin === $iin) {
-                        $result = true;
-                    }
+                    $statues[] = $sign_iin === $iin;
                 } elseif ($rule === 'bin') {
                     $this->validateNotEmpty($bin, 'bin');
 
@@ -106,50 +105,50 @@ class ValidateController extends LayoutController
                         $sign_bin = $ncanode->getBinByXml($xml);
                     }
 
-                    if ($sign_bin === $bin) {
-                        $result = true;
-                    }
+                    $statues[] = $sign_bin === $bin;
                 } elseif ($rule === 'auth') {
                     $this->validateNotEmpty($cms, 'cms');
-                    $result = $ncanode->isAuthCertificate($cms, $verify_ocsp, $verify_crl);
+                    $statues[] = $ncanode->isAuthCertificate($cms, $verify_ocsp, $verify_crl);
                 } elseif ($rule === 'sign') {
                     $this->validateNotEmpty($cms, 'cms');
-                    $result = $ncanode->isSignCertificate($cms, $verify_ocsp, $verify_crl);
+                    $statues[] = $ncanode->isSignCertificate($cms, $verify_ocsp, $verify_crl);
                 } elseif ($rule === 'individual') {
                     if ($cms) {
-                        $result = $ncanode->isIndividual($cms, $verify_ocsp, $verify_crl);
+                        $statues[] = $ncanode->isIndividual($cms, $verify_ocsp, $verify_crl);
                     } else {
-                        $result = $ncanode->isIndividualByXml($xml, $verify_ocsp, $verify_crl);
+                        $statues[] = $ncanode->isIndividualByXml($xml, $verify_ocsp, $verify_crl);
                     }
                 } elseif ($rule === 'employee') {
                     if ($cms) {
-                        $result = $ncanode->isEmployee($cms, $verify_ocsp, $verify_crl);
+                        $statues[] = $ncanode->isEmployee($cms, $verify_ocsp, $verify_crl);
                     } else {
-                        $result = $ncanode->isEmployeeByXml($xml, $verify_ocsp, $verify_crl);
+                        $statues[] = $ncanode->isEmployeeByXml($xml, $verify_ocsp, $verify_crl);
                     }
                 } elseif ($rule === 'ceo') {
                     if ($cms) {
-                        $result = $ncanode->isCeo($cms, $verify_ocsp, $verify_crl);
+                        $statues[] = $ncanode->isCeo($cms, $verify_ocsp, $verify_crl);
                     } else {
-                        $result = $ncanode->isCeoByXml($xml, $verify_ocsp, $verify_crl);
+                        $statues[] = $ncanode->isCeoByXml($xml, $verify_ocsp, $verify_crl);
                     }
                 } elseif ($rule === 'organisation') {
                     if ($cms) {
-                        $result = $ncanode->isOrganization($cms, $verify_ocsp, $verify_crl);
+                        $statues[] = $ncanode->isOrganization($cms, $verify_ocsp, $verify_crl);
                     } else {
-                        $result = $ncanode->isOrganizationByXml($xml, $verify_ocsp, $verify_crl);
+                        $statues[] = $ncanode->isOrganizationByXml($xml, $verify_ocsp, $verify_crl);
                     }
-                }
-
-                if (!$result) {
-                    $this->setContent(['validate' => ['result' => false]]);
                 }
             }
 
-            $this->setContent(['validate' => ['result' => $result]]);
+            $status = false;
+            if (in_array(true, $statues)) {
+                if ($criteria === 'or' || $criteria === 'and' && !in_array(false, $statues)) {
+                    $status = true;
+                }
+            }
+
+            $this->setStatus($status);
         } catch (\Throwable $e) {
             throw $e;
-//            $this->forward('error', 'internalServerError', [$e]);
         }
     }
 }
